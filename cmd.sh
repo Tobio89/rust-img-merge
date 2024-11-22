db8531c226cad7a6b42923a93f90110b07c7803b
Tobio89 <tobio@lunit.io>
2024-11-22 21:56:54 +0900

    feat: calculate bboxes for better fit

---
+++ cmd.sh
@@ -0,0 +1,2 @@
+cargo run -- --red-path ./assets/001-cutoff-tricolor.png --green-path ./assets/002-tissue-seg-unused.png --blue-path ./assets/000-jet-heatmap.png --blue-mode "heatmap" --red-bbox 0 6053 71698 89222  --green-bbox 0 1593 71685 89208 --blue-bbox  0 6053 71698 89222  --out ./output.png
+
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 
-use clap::Parser;
+use clap::{builder::PossibleValue, Parser, ValueEnum};
 use ril::Image;
 use std::path::Path;
 
@@ -24,10 +24,28 @@ enum CollapseColor {
     Blue,
 }
 
+#[derive(Clone)]
 enum CollapseMode {
     Bitmask,
     Heatmap,
     Skip,
+}
+
+impl ValueEnum for CollapseMode {
+    fn value_variants<'a>() -> &'a [Self] {
+        &[
+            CollapseMode::Bitmask,
+            CollapseMode::Heatmap,
+            CollapseMode::Skip,
+        ]
+    }
+    fn to_possible_value(&self) -> Option<PossibleValue> {
+        match self {
+            CollapseMode::Bitmask => Some(PossibleValue::new("bitmask")),
+            CollapseMode::Heatmap => Some(PossibleValue::new("heatmap")),
+            CollapseMode::Skip => Some(PossibleValue::new("skip")),
+        }
+    }
 }
 
 struct CollapseConfig {
@@ -36,33 +54,49 @@ struct CollapseConfig {
     blue: CollapseMode,
 }
 
+struct BBox {
+    min_x: u32,
+    min_y: u32,
+    max_x: u32,
+    max_y: u32,
+}
+
+struct ImageBBoxes {
+    red: BBox,
+    green: BBox,
+    blue: BBox,
+}
+
+struct ImageOffsets {
+    red: ImgSize,
+    green: ImgSize,
+    blue: ImgSize,
+}
+
 #[derive(Debug, Clone, Copy)]
 struct ImgSize(u32, u32);
 
 fn main() {
     let cli: app::Cli = app::Cli::parse();
-    println!("{:?}", cli.red_channel_file_path);
-    println!("{:?}", cli.green_channel_file_path);
-    println!("{:?}", cli.blue_channel_file_path);
-    println!("{:?}", cli.output_file);
 
     /* About test images:
        red: cutoffs, contains three sub-colors
        green: tissue segmentation, contains one sub-color. It is a bit bigger than the others. It is not used in the app.
        blue: heatmap, contains 0-101 values for jet heatmap data.
     */
-    println!(
-        "Red file exists: {}",
-        Path::new(&cli.red_channel_file_path).exists()
-    );
-    println!(
-        "Green file exists: {}",
-        Path::new(&cli.green_channel_file_path).exists()
-    );
-    println!(
-        "Blue file exists: {}",
-        Path::new(&cli.blue_channel_file_path).exists()
-    );
+    let red_file_exists = Path::new(&cli.red_channel_file_path).exists();
+    let green_file_exists = Path::new(&cli.green_channel_file_path).exists();
+    let blue_file_exists = Path::new(&cli.blue_channel_file_path).exists();
+
+    if !red_file_exists {
+        panic!("Red channel file does not exist.");
+    }
+    if !green_file_exists {
+        panic!("Green channel file does not exist.");
+    }
+    if !blue_file_exists {
+        panic!("Blue channel file does not exist.");
+    }
 
     // Prepare image locations
     let args = SourceArgs {
@@ -72,9 +106,15 @@ fn main() {
     };
 
     let config = CollapseConfig {
-        red: CollapseMode::Bitmask,
-        green: CollapseMode::Bitmask,
-        blue: CollapseMode::Heatmap,
+        red: cli.red_mode,
+        green: cli.green_mode,
+        blue: cli.blue_mode,
+    };
+
+    let image_bbox_config = ImageBBoxes {
+        red: validate_bbox(cli.red_bbox).expect("Invalid red bbox"),
+        green: validate_bbox(cli.green_bbox).expect("Invalid green bbox"),
+        blue: validate_bbox(cli.blue_bbox).expect("Invalid blue bbox"),
     };
 
     println!("Loading images...");
@@ -85,28 +125,36 @@ fn main() {
         blue: Image::open(args.blue).expect("Error loading image: "),
     };
 
+    let image_offsets = ImageOffsets {
+        red: calculate_img_offset(
+            loaded_images.red.height(),
+            loaded_images.red.width(),
+            image_bbox_config.red,
+        ),
+        green: calculate_img_offset(
+            loaded_images.green.height(),
+            loaded_images.green.width(),
+            image_bbox_config.green,
+        ),
+        blue: calculate_img_offset(
+            loaded_images.blue.height(),
+            loaded_images.blue.width(),
+            image_bbox_config.blue,
+        ),
+    };
+
+    let min_offsets = get_minimum_offsets(&image_offsets).expect("Could not get minimum offsets");
+
     println!("Images loaded.");
 
     println!("Fitting images...");
     // Get largest image size
     // Used for fitting all images to the same size
-    let largest_img_size = get_largest_img_size(&loaded_images);
-    match largest_img_size {
-        Ok(size) => {
-            println!("Largest image size: {:?}:{:?}", size.0, size.1);
-        }
-        Err(e) => {
-            println!("Error: {:?}", e);
-            panic!("Could not get largest image size");
-        }
-    }
+    let largest_img_size =
+        get_largest_img_size(&loaded_images).expect("Could not get largest image size");
 
-    let largest_img_size = largest_img_size.unwrap();
+    let largest_img_size = largest_img_size;
     let (max_width, max_height) = (largest_img_size.0, largest_img_size.1);
-
-    let (r_width, r_height) = (loaded_images.red.width(), loaded_images.red.height());
-    let (g_width, g_height) = (loaded_images.green.width(), loaded_images.green.height());
-    let (b_width, b_height) = (loaded_images.blue.width(), loaded_images.blue.height());
 
     let blank_image = Image::new(
         max_width,
@@ -125,23 +173,24 @@ fn main() {
         green: blank_image.clone(),
         blue: blank_image.clone(),
     };
-    println!("Large image size: {:?}:{:?}", max_width, max_height);
-    println!("Red image size: {:?}:{:?}", r_width, r_height);
+    // println!("Large image size: {:?}:{:?}", max_width, max_height);
+    // println!("Red image size: {:?}:{:?}", r_width, r_height);
+
     resized_images.red.paste(
-        (max_width - r_width) / 2,
-        (max_height - r_height) / 2,
+        image_offsets.red.0 - min_offsets.0,
+        image_offsets.red.1 - min_offsets.1,
         &loaded_images.red,
     );
 
     resized_images.green.paste(
-        (max_width - g_width) / 2,
-        (max_height - g_height) / 2,
+        image_offsets.green.0 - min_offsets.0,
+        image_offsets.green.1 - min_offsets.1,
         &loaded_images.green,
     );
 
     resized_images.blue.paste(
-        (max_width - b_width) / 2,
-        (max_height - b_height) / 2,
+        image_offsets.blue.0 - min_offsets.0,
+        image_offsets.blue.1 - min_offsets.1,
         &loaded_images.blue,
     );
     println!("Images fitted.");
@@ -238,6 +287,29 @@ fn get_largest_img_size(images: &Images) -> Result<ImgSize, std::io::Error> {
     return Ok(ImgSize(max_width, max_height));
 }
 
+fn get_minimum_offsets(offsets: &ImageOffsets) -> Result<ImgSize, std::io::Error> {
+    let x_offsets = vec![offsets.red.0, offsets.green.0, offsets.blue.0];
+
+    let y_offsets = vec![offsets.red.1, offsets.green.1, offsets.blue.1];
+
+    let min_x_offset = x_offsets.iter().min().copied();
+    let min_y_offset = y_offsets.iter().min().copied();
+
+    let min_x: u32;
+    let min_y: u32;
+
+    match min_x_offset {
+        Some(v) => min_x = v,
+        None => return Err(std::io::Error::other("offsets have no minimum")),
+    }
+    match min_y_offset {
+        Some(v) => min_y = v,
+        None => return Err(std::io::Error::other("offsets have no minimum")),
+    }
+
+    return Ok(ImgSize(min_x, min_y));
+}
+
 fn bit_ize(n: u8) -> u8 {
     if n == 0 {
         return 0;
@@ -293,3 +365,72 @@ fn bit_ize_or_jet_ize(value: u8, mode: &CollapseMode) -> u8 {
         }
     }
 }
+
+fn validate_bbox(bbox: Vec<u32>) -> Result<BBox, std::io::Error> {
+    if bbox.len() != 4 {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "bbox must have 4 values",
+        ));
+    }
+
+    let min_x = bbox[0];
+    let min_y = bbox[1];
+    let max_x = bbox[2];
+    let max_y = bbox[3];
+
+    return Ok(BBox {
+        min_x,
+        min_y,
+        max_x,
+        max_y,
+    });
+}
+
+fn calculate_img_offset(img_height: u32, img_width: u32, img_bbox: BBox) -> ImgSize {
+    // The deal is this:
+    // The BBOX is relative to the full size of the image.
+    // We need to calculate the full size of the image, and then we can figure out what the image's downscale value is.
+    // When we know the downscale, we can figure out the true offset for the image
+    // We will need to return a bbox that is relative to the scaled size of the image
+    // Then maybe we can get the images to be positioned correctly.
+
+    println!(
+        "Bbox: min-x {:?}, min-y {:?}, max-x {:?}, max-y {:?}",
+        img_bbox.min_x, img_bbox.min_y, img_bbox.max_x, img_bbox.max_y
+    );
+
+    let true_width = img_bbox.max_x - img_bbox.min_x;
+    let true_height = img_bbox.max_y - img_bbox.min_y;
+
+    println!(
+        "True width: {:?}, True height: {:?}",
+        true_width, true_height
+    );
+
+    // Should these be floats?
+    let downscale_x: f32 = (true_width as f32 / img_width as f32) as f32;
+    let downscale_y: f32 = (true_height as f32 / img_height as f32) as f32;
+
+    println!(
+        "Downscale x: {:?}, Downscale y: {:?}",
+        downscale_x, downscale_y
+    );
+
+    let scaled_bbox_x = ((img_bbox.min_x as f32) / downscale_x).round() as u32;
+    let scaled_bbox_y = ((img_bbox.min_y as f32) / downscale_y).round() as u32;
+
+    println!("Scaled bbox: {:?}:{:?}", scaled_bbox_x, scaled_bbox_y);
+
+    return ImgSize(scaled_bbox_x, scaled_bbox_y);
+}
+
+/*
+bbox
+
+- find out scaled offsets for each image.
+- find out the largest image
+- take the largest image's offset, and minus
+
+
+*/
